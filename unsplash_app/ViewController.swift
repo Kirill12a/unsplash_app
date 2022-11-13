c8966086e63dc168493865a580afb5eb9fb0e27f
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-11-13 12:17:10 +0300

    Initial Commit

---
+++ unsplash_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unsplash_app
+//
+//  Created by Kirill Drozdov on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
